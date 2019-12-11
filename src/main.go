3dc272994b23611e514789d89ded0bb4e0a2d01c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-12-11 22:50:20 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// You will be using this Trainer type later in the program
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
 
 func main() {
 	settingName := "MainPagePagination"
 	fmt.Println("Settings name is ", settingName)
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }

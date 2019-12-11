9496ea9ef95e1ae46b6b27c9eb578d6264490213
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-12-11 14:24:49 +0200

    Update psake.ps1

--- psake.ps1
+++ psake.ps1
@@ -1,3 +1,39 @@
+Properties {
+    $version = "0.0.1"
+}
+
+Task Build -Depends BuildWinx64, BuildWinx86, BuildLinux64
+
+Task BuildWinx64 -Depends PreBuild {
+    $script:publishWinx64Folder = Join-Path -Path $script:publishFolder -ChildPath "winx64"
+    $outputFile = Join-Path -Path $script:publishWinx64Folder -ChildPath "SettingsService.exe"
+
+    $env:GOOS = "windows"
+    $env:GOARCH = "amd64"
+    Exec { go build -o $outputFile $script:srcFolder }
+}
+
+Task BuildWinx86 -Depends PreBuild {
+    $script:publishWinx86Folder = Join-Path -Path $script:publishFolder -ChildPath "winx86"
+    $outputFile = Join-Path -Path $script:publishWinx86Folder -ChildPath "SettingsService.exe"
+    
+    $env:GOOS = "windows"
+    $env:GOARCH = "386"
+    Exec { go build -o $outputFile $script:srcFolder }
+}
+
+Task BuildLinux64 -Depends PreBuild {
+    $script:publishLinux64Folder = Join-Path -Path $script:publishFolder -ChildPath "linux64"
+    $outputFile = Join-Path -Path $script:publishLinux64Folder -ChildPath "SettingsService"
+
+    $env:GOOS = "linux"
+    $env:GOARCH = "amd64"
+    Exec { go build -o $outputFile $script:srcFolder }
+}
+
+Task PreBuild -Depends Init, Clean, Format, InstallPackages {
+}
+
 Task InstallPackages {
     Exec { go get "go.mongodb.org/mongo-driver" }
 }

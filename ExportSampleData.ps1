713a7765c2695000a7a4344ac7e14199f7b1f2e0
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-12-22 20:08:22 +0200

    Update ExportSampleData.ps1

--- ExportSampleData.ps1
+++ ExportSampleData.ps1
@@ -1,7 +1,9 @@
+Import-Module -Name Mdbc
+
 Connect-Mdbc -ConnectionString 'mongodb://root:9fP30ErG0fBv5R@localhost:52540'
 
 $db = Get-MdbcDatabase -Name 'Settings'
 
-$collection = Get-MdbcCollection -Name 'Defaults' -Database $db
+$collection = Get-MdbcCollection -Name 'DefaultSettings' -Database $db
 
-Get-MdbcData | Export-MdbcData -Path SampleDefaults.json
+Get-MdbcData | Export-MdbcData -Path SampleDefaultSettings.json

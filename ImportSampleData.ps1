fc972e11846814fd7adf495ec20370f9e49a12c9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-12-22 20:07:05 +0200

    Update ImportSampleData.ps1

--- ImportSampleData.ps1
+++ ImportSampleData.ps1
@@ -1,7 +1,9 @@
+Import-Module -Name Mdbc
+
 Connect-Mdbc -ConnectionString 'mongodb://root:9fP30ErG0fBv5R@localhost:52540'
 
 $db = Get-MdbcDatabase -Name 'Settings'
 
-$collection = Get-MdbcCollection -Name 'Defaults' -Database $db
+$collection = Get-MdbcCollection -Name 'DefaultSettings' -Database $db
 
 Import-MdbcData -Path SampleDefaults.json | Set-MdbcData -Collection $collection -Add

9ac7b245aadb84f9c117048fc4b4a73c7cdd87cf
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-12-23 12:28:32 +0200

    Update services.go

--- src/services.go
+++ src/services.go
@@ -16,6 +16,37 @@ func (s *compositeSettingsRetrievalService) GetSettings(path string) map[string]
 
 	m := make(map[string]interface{})
 
+	database, err := getMongoDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	docid, defaultData, err := getDefaultSettings(database, path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(docid)
+	fmt.Println(defaultData)
+
+	userid := "7b803e2d-ee0e-4213-a025-9db732bcbb2e"
+	// userid := "ad2ea197-310a-4832-940c-2935bd6fa511"
+
+	userData, err := getUserSettings(database, docid, user1id)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(userData)
+
+	m, err := mergeSettings(defaultData, user1Data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(m)
+
 	m["path"] = pathParts[0]
 
 	return m
